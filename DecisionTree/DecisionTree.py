0a48395c866c4da886478edda04a36d9d6d5831f
Terrence Ho <terrenceho.books@gmail.com>
2017-05-02 21:00:00 -0700

    Made cost function for splits

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -13,9 +13,27 @@ class DecisionTree(object):
 
     def fit(self, X, y):
     # function to make decision tree
+        return 0
 
     def prune_tree(self, max_depth):
         current_tree = self.tree
 
     def predict(x):
     # function to make a prediction from said tree
+        return 0
+
+    def gini_index(self, groups, class_values): #cost function to split data
+        gini = 0
+        for class_value in class_values:
+            for group in groups:
+                size=len(group)
+                if size == 0:
+                    continue
+                proportion = [row[-1] for row in group].count(class_value) / float(size)
+                gini += proportion* (1.0 - proportion)
+        return gini
+
+
+Tree = DecisionTree()
+print(Tree.gini_index([[[1, 1], [1, 0]], [[1, 1], [1, 0]]], [0, 1]))
+print(Tree.gini_index([[[1, 0], [1, 0]], [[1, 1], [1, 1]]], [0, 1]))
