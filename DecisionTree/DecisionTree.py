def1ac5bfa92c628e9ac39600141ddcceee7a783
Terrence Ho <terrenceho.books@gmail.com>
2017-05-05 19:33:30 -0700

    Added Eval functions

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -1,12 +1,3 @@
-
-
-# class TreeNode(object):
-#     """A node for the decision tree"""
-#     def __init__(self, isLeaf=False):
-#         self.data = None
-#         self.isLeaf = False
-
-
 class DecisionTree(object):
     # needs entropy critierion
     def __init__(self):
@@ -102,5 +93,62 @@ class DecisionTree(object):
             self.print_tree(node['right'], depth+1)
         else:
             print('%s[%s]' % ((depth*' ', node)))
+
+    # Split a dataset into k folds
+    def cross_validation_split(self, dataset, n_folds):
+	dataset_split = list()
+	dataset_copy = list(dataset)
+	fold_size = int(len(dataset) / n_folds)
+	for i in range(n_folds):
+            fold = list()
+            while len(fold) < fold_size:
+                index = randrange(len(dataset_copy))
+                fold.append(dataset_copy.pop(index))
+            dataset_split.append(fold)
+	return dataset_split
+
+    def evaluate_algorithm(self, dataset, algorithm, n_folds, *args):
+	folds = cross_validation_split(dataset, n_folds)
+	scores = list()
+	for fold in folds:
+            train_set = list(folds)
+            train_set.remove(fold)
+            train_set = sum(train_set, [])
+            test_set = list()
+            for row in fold:
+		row_copy = list(row)
+		test_set.append(row_copy)
+		row_copy[-1] = None
+            predicted = algorithm(train_set, test_set, *args)
+            actual = [row[-1] for row in fold]
+            accuracy = accuracy_metric(actual, predicted)
+            scores.append(accuracy)
+	return scores
+
+
+def load_csv(filename):
+    file = open(filename, "rb")
+    lines = reader(file)
+    dataset = list(lines)
+    return dataset
+
+def str_column_to_float(dataset, column):
+    for row in dataset:
+        row[column] = float(row[column].strip())
+
+def accuracy_metric(actual, predicted):
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[i]:
+            correct += 1
+    return correct / float(len(actual)) * 100.0
+
+
+# class TreeNode(object):
+#     """A node for the decision tree"""
+#     def __init__(self, isLeaf=False):
+#         self.data = None
+#         self.isLeaf = False
+
 
 
