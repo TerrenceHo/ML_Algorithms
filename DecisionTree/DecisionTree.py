b6a6147c2013800e04a7de2fdff8cd05cb76600a
Terrence Ho <terrenceho.books@gmail.com>
2017-05-10 22:57:23 -0700

    Refactoring code

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -1,42 +1,30 @@
-from random import seed
-from random import randrange
-from csv import reader
-
+# Decision Tree
 
 class DecisionTree(object):
-    # needs entropy critierion
-    def __init__(self):
+    def __init__(self, max_depth, min_size):
         self.root = None
+        self.max_depth = max_depth
+        self.min_size = min_size
 
-    def prune_tree(self, max_depth): # Future function to prune tree
-        current_tree = self.tree
+    def gini_index(self, groups, class_values):
+        # Return a cost funtion between 0 <= x <= 1, where 0 means you have split
+        # the data 50/50, and 1 is the best possible split (i.e., no split).  
 
-    def predict(self, node, row):
-    # function to make a prediction from said tree
-        if row[node['index']] < node['value']:
-            if isinstance(node['left'], dict):
-                return self.predict(node['left'], row)
-            else:
-                return node['left']
-
-        else:
-            if isinstance(node['right'], dict):
-                return self.predict(node['right'], row)
-            else:
-                return node['right']
-
-    def gini_index(self, groups, class_values): #cost function to split data
-        gini = 0
+        gini = 0.0
         for class_value in class_values:
             for group in groups:
-                size=len(group)
+                size = len(group)
                 if size == 0:
                     continue
                 proportion = [row[-1] for row in group].count(class_value) / float(size)
-                gini += proportion* (1.0 - proportion)
+                gini += (proportion * (1.0 - proportion))
         return gini
 
     def test_split(self, index, value, dataset):
+        # Generate two lists, one list where the value of the index we are looking
+        # at is less than the passed in value, and the right list is greater than
+        # passed in value
+
         left, right = list(), list()
         for row in dataset:
             if row[index] < value:
@@ -46,121 +34,97 @@ class DecisionTree(object):
         return left, right
 
     def get_split(self, dataset):
+        # Generate the best split, by calling test_split to repeatedly test out
+        # various splits and choosing the one with the least gini_index cost.
+
+        best_index, best_value, best_score, best_groups = 999,999,999,None
         class_values = list(set(row[-1] for row in dataset))
-        b_index, b_value, b_score, b_groups = 999,999,999,None
         for index in range(len(dataset[0]) -1):
             for row in dataset:
                 groups = self.test_split(index, row[index], dataset)
                 gini = self.gini_index(groups, class_values)
-                print('X%d < %.3f Gini=%.3f' % ((index+1), row[index], gini))
-                if gini < b_score:
-                    b_index, b_value, b_score, b_groups = index, row[index], gini, groups
-        return {'index':b_index, 'value':b_value, 'groups':b_groups}
+                if gini < best_score:
+                    best_index, best_value, best_score, best_groups = index, \
+                    row[index], gini, groups
 
-    def to_terminal(self,group):
-        outcomes = [row[-1] for row in group]
-        return max(set(outcomes), key=outcomes.count)
+        return {'index':best_index, 'value':best_value, 'groups':best_groups}
 
     def split(self, node, max_depth, min_size, depth):
+        # Recursing splitting function to split our data.  We stop splitting when
+        # we hit the maximum depth size, and/or the data does not split.
+        # Otherwise, we call get_split function on the left and right side.  If
+        # the groups on either side get too small, then we call the terminal node
+        # to make it a leaf node.
+
         left, right = node['groups']
         del(node['groups'])
-        # check of no splits, BASE case
+        # check for a no split
         if not left or not right:
             node['left'] = node['right'] = self.to_terminal(left + right)
             return
-        # check for max depth
+	# check for max depth
         if depth >= max_depth:
             node['left'], node['right'] = self.to_terminal(left), self.to_terminal(right)
             return
-        # process left child
+	# process left child
         if len(left) <= min_size:
             node['left'] = self.to_terminal(left)
         else:
             node['left'] = self.get_split(left)
             self.split(node['left'], max_depth, min_size, depth+1)
-        # process right child
+	# process right child
         if len(right) <= min_size:
             node['right'] = self.to_terminal(right)
         else:
             node['right'] = self.get_split(right)
             self.split(node['right'], max_depth, min_size, depth+1)
 
-    def build_tree(self, train, max_depth, min_size):
-        root = self.get_split(train)
-        self.split(root, max_depth, min_size, 1)
-        return root
+    def to_terminal(self, group):
+        # Only to be used at terminal/leaf nodes of the tree, if we decide that we
+        # will stop adding new splits and branches to the tree.  Sets all
+        # class_values of that group to the same, whatever is max.
 
-    def print_tree(self, node, depth=0):
+        outcomes = [row[-1] for row in group]
+        return max(set(outcomes), key=outcomes.count)
+
+    def fit(self, train):
+        # train is the dataset
+        # max_depth is how deep we want to go
+        # min_size is how small the terminal node can be
+        # function that recursively calls split to build a tree
+
+        self.root = self.get_split(train)
+        self.split(self.root, self.max_depth, self.min_size, 1)
+
+    def print_tree(self, depth = 0):
+        # Calls recursive function to print out how the tree looks like
+        self.print_tree_func(self.root, depth)
+
+    def print_tree_func(self, node, depth = 0):
+        # Recursive function that prints out how tree looks like.  Prints out left
+        # and right nodes, and if there are no more, prints out split values.
         if isinstance(node, dict):
-            print('%s[X%d < %.3f]' % ((depth*' ', (node['index']+1),
-                  node['value'])))
-            self.print_tree(node['left'], depth+1)
-            self.print_tree(node['right'], depth+1)
+            print('%s[X%d < %.3f]' % ((depth*' ', (node['index']+1), \
+                                       node['value'])))
+            self.print_tree_func(node['left'], depth+1)
+            self.print_tree_func(node['right'], depth+1)
         else:
             print('%s[%s]' % ((depth*' ', node)))
 
-    # Split a dataset into k folds
-    def cross_validation_split(self, dataset, n_folds):
-        dataset_split = list()
-        dataset_copy = list(dataset)
-        fold_size = int(len(dataset) / n_folds) 
-        for i in range(n_folds):
-            fold = list()
-            while len(fold) < fold_size:
-                index = randrange(len(dataset_copy))
-                fold.append(dataset_copy.pop(index))
-            dataset_split.append(fold)
-        return dataset_split
-
-    def evaluate_algorithm(self, dataset, algorithm, n_folds, *args):
-        folds = self.cross_validation_split(dataset, n_folds)
-        scores = list()
-        for fold in folds:
-            train_set = list(folds)
-            train_set.remove(fold)
-            train_set = sum(train_set, [])
-            test_set = list()
-            for row in fold:
-                row_copy = list(row)
-                test_set.append(row_copy)
-                row_copy[-1] = None
-            predicted = algorithm(train_set, test_set, *args)
-            actual = [row[-1] for row in fold]
-            accuracy = accuracy_metric(actual, predicted)
-            scores.append(accuracy)
-        return scores
-
-    def decision_tree(self, train, test, max_depth, min_size):
-        tree = self.build_tree(train, max_depth, min_size)
-        predictions = list()
-        for row in test:
-            prediction = self.predict(tree, row)
-            predictions.append(prediction)
-        return predictions
-
-def load_csv(filename):
-    file = open(filename, "r")
-    lines = reader(file)
-    dataset = list(lines)
-    return dataset
-
-def str_column_to_float(dataset, column):
-    for row in dataset:
-        row[column] = float(row[column].strip())
-
-def accuracy_metric(actual, predicted):
-    correct = 0
-    for i in range(len(actual)):
-        if actual[i] == predicted[i]:
-            correct += 1
-    return correct / float(len(actual)) * 100.0
-
-
-# class TreeNode(object):
-#     """A node for the decision tree"""
-#     def __init__(self, isLeaf=False):
-#         self.data = None
-#         self.isLeaf = False
-
-
+    def predict(self, row, node = None):
+        # function that predicts based off the node that is passed in, the default
+        # of which is root node of Tree.  Recursive function that traverses the
+        # tree until it hits terminal node, then returns the predicted value
+        if node == None:
+            node = self.root
+        if row[node['index']] < node['value']:
+            if isinstance(node['left'], dict):
+                return self.predict(node['left'], row)
+            else:
+                return node['left']
+        else:
+            if isinstance(node['right'], dict):
+                return self.predict(node['right'], row)
+            else:
+                return node['right']
 
