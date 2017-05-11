013af54c9c6ce1873c88272c46afb55c5ba6475e
Terrence Ho <terrenceho.books@gmail.com>
2017-05-11 10:53:53 -0700

    Added utils file, to eval my algorithm

---
+++ DecisionTree/utils.py
@@ -0,0 +1,39 @@
+# various utility functions that can be used for many ml problems
+
+from random import seed
+from random import randrange
+from csv import reader
+
+def load_csv(filename):
+    # returns lines of dataset as a list
+    file = open(filename, 'r')
+    lines = reader(file)
+    dataset = list(lines)
+    return dataset
+
+def str_column_to_float(dataset, column):
+    for row in dataset:
+        row[column] = float(row[column].strip())
+
+def cross_validation_split(dataset, n_folds):
+    dataset_split = list()
+    dataset_copy = list(dataset)
+    fold_size = int(len(dataset) / n_folds)
+    for i in range(n_folds):
+        fold = list()
+        while len(fold) < fold_size:
+            index = randrange(len(dataset_copy))
+            fold.append(dataset_copy.pop(index))
+
+        dataset_split.append(fold)
+    return dataset_split
+
+def accuracy(actual, predicted):
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[i]:
+            correct += 1
+    return correct / float(len(actual)) * 100.0
+
+
+
